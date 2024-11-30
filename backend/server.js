36d6a06e9f0605ae2a02580e198e506d753e164e
Ujjwal <estyujjtwal512@gmail.com>
2024-11-30 17:55:33 +0530

    enable cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const createUserTable = require("./src/db/user/user-table");
 const userRoutes = require("./src/user");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
@@ -8,6 +9,7 @@ const port = 3000;
 async function main() {
   await createUserTable();
 
+  app.use(cors());
   app.use(express.json());
 
   app.use("/users", userRoutes);
