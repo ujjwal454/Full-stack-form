51bab924c512662e14276958a57fbb5cfb8a4864
Ujjwal <estyujjtwal512@gmail.com>
2024-11-30 15:55:03 +0530

    updated config

--- backend/src/db/config.js
+++ backend/src/db/config.js
@@ -1,10 +1,10 @@
 const { Client } = require("pg");
 
 const client = new Client({
-  host: "localhost",
-  user: "ujjwal",
-  password: "1234",
-  database: "testDB",
+  host: "dpg-ct5ee23tq21c7398en80-a",
+  user: "form_backend_user",
+  password: "Ofga1arB4amcZgcUdNURzpjVUtgE4ftW",
+  database: "form_backend",
   port: 5432,
 });
 
